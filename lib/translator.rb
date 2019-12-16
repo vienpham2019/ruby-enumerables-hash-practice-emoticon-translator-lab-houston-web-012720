2f95196cf07f511058ed520ae97d5240dd60cf07
vien pham <vienpham2019@gmail.com>
2019-12-16 22:36:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,20 @@
 # require modules here
+require "yaml"
+require "pry"
 
-def load_library
+def load_library(path)
   # code goes here
+  emoticons = YAML.load_file(path)
+  new_hash = {}
+  new_hash["get_emoticon"] = {}
+  new_hash["get_meaning"] = {}
+  emoticons.each{|e,array|
+    new_hash["get_meaning"][array[1]] = e
+    new_hash["get_emoticon"][array[0]] = array[1]
+  }
+  new_hash
 end
-
+  
 def get_japanese_emoticon
   # code goes here
 end
